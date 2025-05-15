74b865768ee88c5c7dd56cdf7fd537650c93edd4
MATRIX30 <matrixboy30@gmail.com>
2025-05-16 00:14:35 +0100

    finish task 0. Write Complex Queries with Joins - lEFT join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,3 +3,11 @@
 SELECT * FROM Booking  b 
 INNER JOIN User  u
 ON b.user_id = u.user_id;
+
+
+-- query using aLEFT JOIN to retrieve all properties 
+-- and their reviews, including properties that have no reviews.
+
+SELECT * FROM Property p 
+LEFT JOIN Review r 
+ON p.property_id = r.property_id;
