73021769607dffc7267f5061d63932bc9cc41361
MATRIX30 <matrixboy30@gmail.com>
2025-05-15 10:40:18 +0100

    finish task 2. Design Database Schema (DDL)

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,1 +1,79 @@
-how
+--- This sql DDL commands will create the database schema for the Airbnb project specification
+
+--- User Schema
+CREATE TABLE IF NOT EXISTS User(
+    user_id CHAR(36) PRIMARY KEY,
+    first_name VARCHAR(256) NOT NULL,
+    last_name VARCHAR(256) NOT NULL,
+    email VARCHAR(25) UNIQUE NOT NULL,
+    password_hash VARCHAR(55) NOT NULL,
+    phone_number VARCHAR(20) NULL,
+    role ENUM('guest', 'host', 'admin') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    -- we can ommit the next instruction because UNIQUE and PRIMARY KEY
+    -- would automatically index the column
+    -- INDEX index_email (email)
+);
+
+-- PROPERTY TABLES
+CREATE TABLE IF NOT EXISTS Property(
+    property_id CHAR(36) PRIMARY KEY,
+    host_id CHAR(36) NOT NULL,
+    name VARCHAR(256)NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR(256) NOT NULL,
+    pricepernight DECIMAL NOT Null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (host_id) REFERENCES User(user_id) ON DELETE CASCADE
+)
+
+-- Booking Table
+CREATE TABLE IF NOT EXISTS Booking (
+    booking_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX idx_booking_id (booking_id),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
+);
+
+-- Payment Table
+CREATE TABLE IF NOT EXISTS Payment (
+    payment_id CHAR(36) PRIMARY KEY,
+    booking_id CHAR(36) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    INDEX idx_payment_id (payment_id),
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id) ON DELETE CASCADE
+);
+-- Review Table
+CREATE TABLE IF NOT EXISTS Review (
+    review_id CHAR(36) PRIMARY KEY,
+    property_id CHAR(36) NOT NULL,
+    user_id CHAR(36) NOT NULL,
+    rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX idx_review_id (review_id),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE
+);
+
+-- Message Table
+CREATE TABLE IF NOT EXISTS Message (
+    message_id CHAR(36) PRIMARY KEY,
+    sender_id CHAR(36) NOT NULL,
+    recipient_id CHAR(36) NOT NULL,
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    INDEX idx_message_id (message_id),
+    FOREIGN KEY (sender_id) REFERENCES User(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (recipient_id) REFERENCES User(user_id) ON DELETE CASCADE
+);
