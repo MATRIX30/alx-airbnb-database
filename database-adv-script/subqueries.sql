5c34d038d92340f248cb39eba9574abc1a35ab14
MATRIX30 <matrixboy30@gmail.com>
2025-05-16 04:45:00 +0100

    finish task 1. Practice Subqueries correlated

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -8,3 +8,13 @@ WHERE (
     FROM Review r
     WHERE r.property_id = p.property_id
 ) > 4.0 ;
+
+-- correlated subquery to find users who have made
+-- more than 3 bookings.
+SELECT * FROM User u
+WHERE u.user_id IN (
+    SELECT b.user_id
+    FROM Booking b
+    GROUP BY b.user_id
+    HAVING COUNT(b.booking_id) > 3
+)
