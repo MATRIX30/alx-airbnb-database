d51e9ca8c1bba790f40a814f734f3a583b53ca69
MATRIX30 <matrixboy30@gmail.com>
2025-05-16 04:03:20 +0100

    finish task 0. Write Complex Queries with Joins - FULL outer join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -11,3 +11,13 @@ ON b.user_id = u.user_id;
 SELECT * FROM Property p 
 LEFT JOIN Review r 
 ON p.property_id = r.property_id ORDER BY p.name;
+
+
+
+-- query using a FULL OUTER JOIN to retrieve all users and
+--  all bookings, even if the user has no booking or a booking is not linked to a user.
+
+SELECT * FROM User u
+FULL OUTER JOIN Booking b
+ON u.user_id = b.user_id
+ORDER BY u.email
