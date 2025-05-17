033d943b9ffe0656e3d8702b3b3f1cb6188bc714
MATRIX30 <matrixboy30@gmail.com>
2025-05-17 05:27:48 +0100

    optimized

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,6 +1,6 @@
 -- Write an initial query that retrieves all bookings along with the user
 -- details, property details, and payment details
-SELECT
+EXPLAIN ANALYZE SELECT
     b.*,
     u.first_name,
     u.last_name,
