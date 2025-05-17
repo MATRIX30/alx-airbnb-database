c260bb06d66f7aca7afc83b64e6ba48f606cbe69
MATRIX30 <matrixboy30@gmail.com>
2025-05-17 05:11:17 +0100

    3. Implement Indexes for Optimization

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,23 @@
 -- creating index to optimize database performance
+
+
+
+EXPLAIN ANALYZE SELECT * FROM User WHERE user_id = 123;
+EXPLAIN ANALYZE SELECT * FROM User WHERE email = 'test@example.com';
+EXPLAIN ANALYZE SELECT * FROM User WHERE phone_number = '1234567890';
+EXPLAIN ANALYZE SELECT * FROM User WHERE role = 'host';
+
+-- Booking table
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE status = 'confirmed';
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE total_price > 1000;
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE property_id = 42;
+EXPLAIN ANALYZE SELECT * FROM Booking WHERE user_id = 123;
+
+-- Property table
+EXPLAIN ANALYZE SELECT * FROM Property WHERE propert_id = 99;
+EXPLAIN ANALYZE SELECT * FROM Property WHERE host_id = 77;
+EXPLAIN ANALYZE SELECT * FROM Property WHERE name = 'Cozy Apartment';
+EXPLAIN ANALYZE SELECT * FROM Property WHERE pricepernight < 200;
 -- optimize User table
 CREATE INDEX idx_user_user_id ON User(user_id);
 CREATE INDEX idx_user_email ON User(email);
